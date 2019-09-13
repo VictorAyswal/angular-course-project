450f28c3729d0ba2a9537e95a9d86caa435f9ac0
VictorAyswal <victorayswal@gmail.com>
2019-09-13 12:43:00 +0200

    refactor app to apply lazy-loading

---
+++ src/app/shipping/shipping.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShippingRoutingModule } from './shipping-routing.module';
+import { ShippingContainerComponent } from './shipping-container/shipping-container.component';
+import { ShippingItemComponent } from './component/shipping-item/shipping-item.component';
+
+
+@NgModule({
+  declarations: [
+    ShippingContainerComponent,
+    ShippingItemComponent
+  ],
+  imports: [
+    CommonModule,
+    ShippingRoutingModule
+  ]
+})
+export class ShippingModule { }
